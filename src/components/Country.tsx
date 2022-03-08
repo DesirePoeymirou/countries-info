1b8535808bbae007e823cc6a1243d21ceec855cd
Désiré Poëymirou <desirepoeymirou@gmail.com>
2022-03-08 01:03:00 -0300

    Last touches

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,26 +1,37 @@
 import { useQuery } from "@apollo/client";
 import { GetCountry } from "../queries";
 import { useParams } from "react-router-dom";
+import styles from "../styles/Country.module.css";
 
 const Country: React.FC = () => {
   const { countryCode } = useParams();
   const { loading, error, data } = useQuery(GetCountry(countryCode!));
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading country.</p>;
-
+  console.log(data.country);
   return (
-    <>
-      <div>Name: {data.country.name}</div>
-      <div>Continent: {data.country.continent.name}</div>
-      <div>Capital: {data.country.capital}</div>
-      <div>Currencies: {data.country.currency}</div>
-      <div>Languages: </div>
-      {data.country.languages.map(
-        (lang: { __typename: string; name: string }) => (
-          <div key={lang.name}>{lang.name}</div>
-        )
-      )}
-    </>
+    <div className={styles.country}>
+      <div className={styles.title}>
+        <div>Name :</div>
+        <div>Continent :</div>
+        <div>Capital :</div>
+        <div>Currencies :</div>
+        <div>Languages :</div>
+      </div>
+      <div className={styles.data}>
+        <div>{data.country.name}</div>
+        <div>{data.country.continent.name}</div>
+        <div>{data.country.capital}</div>
+        <div>{data.country.currency}</div>
+        <div>
+          {data.country.languages.map(
+            (lang: { __typename: string; name: string }) => (
+              <span key={lang.name}>{`${lang.name} `}</span>
+            )
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
